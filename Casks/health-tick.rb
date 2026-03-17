a4fa3b367bace90f2598edbff49058dc9765041e
Timothy.Ge <lifedever@gmail.com>
2026-03-17 22:09:37 +0800

    Update HealthTick to v1.4.4

--- Casks/health-tick.rb
+++ Casks/health-tick.rb
@@ -1,9 +1,9 @@
 cask "health-tick" do
   arch arm: "Apple-Silicon", intel: "Intel"
 
-  version "1.4.3"
-  sha256 arm:   "073e480991f4d95007241e3935f0cdbfffa1eb8400e8581979240b690479156c",
-         intel: "641e098bb1e8817e0e98c3f3e03dc3d76ade977f9424a5e13205df380bb71ad7"
+  version "1.4.4"
+  sha256 arm:   "9093be17c568baea6193795d146f9143cf6abf4ffbc904aad3f84f7489b51937",
+         intel: "344a1233caee10f0e6acd8599185ce29c1b4cd331f98e1f5eefc980d8a2dad79"
 
   url "https://github.com/lifedever/health-tick-release/releases/download/v#{version}/HealthTick-v#{version}-#{arch}.dmg"
   name "HealthTick"
