23ab878905ccedcf72d7dfc1dc1073b5335b4f5d
Timothy.Ge <gefangshuai@outlook.com>
2026-04-01 15:39:44 +0800

    Update PasteMemo cask to v1.0.0

--- Casks/pastememo.rb
+++ Casks/pastememo.rb
@@ -1,11 +1,11 @@
 cask "pastememo" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.5.2"
-  sha256 arm:   "e5f4fb6a58b56cce839e0ef3eeb1144c2c11dc5f724d6c2958734e51e8215ebd",
-         intel: "5e320d70910415f2128bca338840e63742d0d4928d660e3a0c9d55a0fcf66641"
+  version "1.0.0"
+  sha256 arm:   "ab35b68f4b049c0e79c27172c32f875987fc7194580a38440e57ccf758745972",
+         intel: "f1381d8edb2ec951368198dd2204418e4213bb10a2afd65e60869a20b38c929b"
 
-  url "https://github.com/lifedever/PasteMemo/releases/download/v#{version}/PasteMemo-#{version}-#{arch}.dmg"
+  url "https://github.com/lifedever/PasteMemo-app/releases/download/v#{version}/PasteMemo-#{version}-#{arch}.dmg"
   name "PasteMemo"
   desc "Smart clipboard manager for macOS"
   homepage "https://www.lifedever.com/PasteMemo/"
