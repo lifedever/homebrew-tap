a69007aa4035fbb7330658da92c085e0c67e688f
Timothy.Ge <gefangshuai@outlook.com>
2026-04-04 11:34:37 +0800

    Update PasteMemo cask to v1.2.1

--- Casks/pastememo.rb
+++ Casks/pastememo.rb
@@ -1,9 +1,9 @@
 cask "pastememo" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.2.0"
-  sha256 arm:   "c0e4c3b271539e3c8af17e0a157c480c937d1f03b182ee8a41d8e5727e519dd3",
-         intel: "a5c1101f6857d0d50836e5c88a4364d2ec681188503d0239e5fb43a888cf7f03"
+  version "1.2.1"
+  sha256 arm:   "14f69c7fab45d4fe28ac03c2b1308e093e45e19f424a2fd43a02b17b652e0f91",
+         intel: "ed7dad69f573c13a89418056521d115f94a26d9b640824e35a6b7e1c7169acf1"
 
   url "https://github.com/lifedever/PasteMemo-app/releases/download/v#{version}/PasteMemo-#{version}-#{arch}.dmg"
   name "PasteMemo"
