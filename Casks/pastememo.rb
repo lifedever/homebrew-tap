d850c448a2191c053a2d2903734e3a358f9a642f
Timothy.Ge <gefangshuai@outlook.com>
2026-04-16 20:57:25 +0800

    Fix PasteMemo cask v1.3.1 (auto-publish regex hit wrong lines)

--- Casks/pastememo.rb
+++ Casks/pastememo.rb
@@ -1,9 +1,9 @@
 cask "pastememo" do
-  arch arm:   "200567ebd71b09571dda4f28b0fa78597ce70be0147f706f1741a93bea5a6541", intel: "475b8149a1b292a9c4870320acc8a9561f16c2c9b0e693ad8b44113a6b4e335c"
+  arch arm: "arm64", intel: "x86_64"
 
   version "1.3.1"
-  sha256 arm:   "f9fdb3639147c6514786aa3da662097cf9a35130ad54c055df9d23fae69acd37",
-         intel: "30092e7407e54328be88be36c7128ab0568bc59b370a4e447feb626fdeae2c26"
+  sha256 arm:   "200567ebd71b09571dda4f28b0fa78597ce70be0147f706f1741a93bea5a6541",
+         intel: "475b8149a1b292a9c4870320acc8a9561f16c2c9b0e693ad8b44113a6b4e335c"
 
   url "https://github.com/lifedever/PasteMemo-app/releases/download/v#{version}/PasteMemo-#{version}-#{arch}.dmg"
   name "PasteMemo"
