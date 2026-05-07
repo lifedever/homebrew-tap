bf13bcd31c1d10740019f6655dae150b4161ead1
lifedever <lifedever@gmail.com>
2026-05-07 23:01:27 +0800

    filelens 1.1.1

--- Casks/filelens.rb
+++ Casks/filelens.rb
@@ -1,6 +1,6 @@
 cask "filelens" do
-  version "1.1.0"
-  sha256 "af401710ec08c62f75198c5dbe1f3f272406bb2675caa9d0698e4e17eb2183df"
+  version "1.1.1"
+  sha256 "7b8f75429883c97fbb5838bd4f32c0b10114ae1112cb56b70a5bb7dcd2c782ff"
 
   url "https://github.com/lifedever/file-lens/releases/download/v#{version}/FileLens-#{version}-universal.dmg"
   name "FileLens"
