c283db26b77e46085d0d437b017fd6685f88657a
Lifedever <lifedever@gmail.com>
2026-06-04 13:07:12 +0800

    health-tick 1.6.15

--- Casks/health-tick.rb
+++ Casks/health-tick.rb
@@ -1,9 +1,9 @@
 cask "health-tick" do
   arch arm: "Apple-Silicon", intel: "Intel"
 
-  version "1.6.13"
-  sha256 arm:   "22c12692f81518a728b64abd5795b500104502d92fbb34100295e656ec432f65",
-         intel: "3defc8a2e44f32d801c1b169639ef69f88761b668618aef57b7507272f37beaa"
+  version "1.6.15"
+  sha256 arm:   "8d9ba7e43f7bf6907355f85347ffce3f267632d1fb812bdb62a692fb71afca92",
+         intel: "9a5bba8c1d6d4c962fdc4411d1c3f3d2a54b06aeb445ce23d42810e44acae2fd"
 
   url "https://github.com/lifedever/health-tick-release/releases/download/v#{version}/HealthTick-v#{version}-#{arch}.dmg"
   name "HealthTick"
