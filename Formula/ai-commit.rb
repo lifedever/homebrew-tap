b885b95e36ad96d41e4a1af716cb175afc8bdc09
lifedever <lifedever@gmail.com>
2026-07-10 11:26:20 +0800

    ai-commit 1.4.0

--- Formula/ai-commit.rb
+++ Formula/ai-commit.rb
@@ -1,8 +1,8 @@
 class AiCommit < Formula
   desc "AI-powered Git commit message generator using LLM"
   homepage "https://github.com/lifedever/ai-commit"
-  url "https://github.com/lifedever/ai-commit/archive/refs/tags/v1.3.7.tar.gz"
-  sha256 "e8af3db8f6dad8c7dd62d3e6bb683b40f6c8a496dcc4405bdb926d65e0e4dd7b"
+  url "https://github.com/lifedever/ai-commit/archive/refs/tags/v1.4.0.tar.gz"
+  sha256 "21481425bf1b7fd4f1417c7d451160d543712ac37dd3f50cdc4524806231c238"
   license "MIT"
 
   depends_on "node"
