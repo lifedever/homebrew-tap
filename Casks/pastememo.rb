4b307101f048f5c4dc8637613b434dd3b8fa7382
lifedever <lifedever@gmail.com>
2026-08-04 13:48:20 +0800

    pastememo 1.7.12

--- Casks/pastememo.rb
+++ Casks/pastememo.rb
@@ -1,9 +1,9 @@
 cask "pastememo" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.7.11"
-  sha256 arm:   "64acc2393e8e4394b81c9371a53bd623c3ae37482321dfff8f1b925ef7f29a7e",
-         intel: "5595aaf7e220785d01de0f1cbfc56fcb49fb5ce6d7789a2eb6cc0537508a2bb0"
+  version "1.7.12"
+  sha256 arm:   "a16b5ffc1034a9c2edab3bd561aa388760fe2f37a030b059b95c12293135ffd3",
+         intel: "056d562244949221ddb74487640e161437b10df90d82858c4a661adebe60c129"
 
   url "https://github.com/lifedever/PasteMemo-app/releases/download/v#{version}/PasteMemo-#{version}-#{arch}.dmg"
   name "PasteMemo"
