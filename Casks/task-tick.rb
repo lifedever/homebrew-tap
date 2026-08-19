eacf0483fb1a3bcbbe82ec3365b18987bde238eb
lifedever <lifedever@gmail.com>
2026-08-19 10:58:43 +0800

    Update TaskTick cask to v1.18.0

--- Casks/task-tick.rb
+++ Casks/task-tick.rb
@@ -1,9 +1,9 @@
 cask "task-tick" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.17.0"
-  sha256 arm:   "158a94ff70486f4ff075199af1d088f7d9319ae20a5c3543ae04bc08126b629c",
-         intel: "62d921b095eb0d7da7b4da9b6e7d0286ab547c5956254a2d1c236666203bbc4b"
+  version "1.18.0"
+  sha256 arm:   "2712d27f0b1b2fdfe692339ee4b832f4ebfad975be0579a05087feb6afc80c3c",
+         intel: "d66df78d92dee218d08f6044a5bb802fcec30a74347b68fd80d47053df0262dc"
 
   url "https://github.com/lifedever/TaskTick/releases/download/v#{version}/TaskTick-#{version}-#{arch}.dmg"
   name "TaskTick"
